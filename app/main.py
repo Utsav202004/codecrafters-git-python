b7304dc0dab7858b48f77a6f62de34637b0b0ab4
Utsav Goyal <goyalutsav2004@gmail.com>
2025-09-13 21:46:40 +0530

    Implement support for basic pipelines involving two external commands

--- app/main.py
+++ app/main.py
@@ -5,13 +5,16 @@ import os
 def main():
     
     command = sys.argv[1]
+
     if command == "init":
+
         os.mkdir(".git")
         os.mkdir(".git/objects")
         os.mkdir(".git/refs")
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+        
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
