93e8bcdef5ac81eb0f2a0f9a6999ac3c4f9c144d
Utsav Goyal <goyalutsav2004@gmail.com>
2025-09-27 00:10:56 +0530

    Final Refactoring

--- app/main.py
+++ app/main.py
@@ -37,17 +37,17 @@ class Git:
 
         flag = args[0]
         hash_str = args[1]
-        object_path = os.path.join(self.git_dir, 'objects', hash_str[:2], hash_str[2:]) # the object associated with the given hash
+        object_path = os.path.join(self.git_dir, Git.OBJECTS_DIR, hash_str[:2], hash_str[2:]) # the object associated with the given hash
         
         # Need content of file at the path - zlib decompression
         content = self._get_object_content(object_path)
 
         # Content format - <object-type> <size>\x00<content>
-        if content:
-            header, _ , body = content.partition(b'\x00')
-        else:
-            print("Error with reading file")
+        if content is None:
+            print(f"fatal: Not a valid object name {hash_str}", file=sys.stderr)
             sys.exit(1)
+
+        header, _ , body = content.partition(b'\x00')
             
         if flag == '-p':
             print(body.decode('utf-8'), end='') # content in byte format - converting to string format
@@ -58,28 +58,31 @@ class Git:
     def hash_object(self, args):
         if len(args) < 2 or args[0] != '-w':
             print(f"Usage: hash-object -w file-name", file=sys.stderr)
+            sys.exit(1)
 
         if not os.path.exists(args[1]):
             print(f"fatal: file does not exist.")
+            sys.exit(1)
 
         # Need to : Read file -> Add header to file -> Calculate the sha1 hash -> compress file using zlib -> write to the objects/hash[:2] path
         try:
-            with open(args[1], 'r') as f:
-                file_content = f.read()
+            with open(args[1], 'rb') as f:
+                file_content_bytes= f.read()
         except Exception as e:
             print(f"Error in reading the file: {e}", file=sys.stderr)
             sys.exit(1)
 
-        file_content_size = os.path.getsize(args[1])
-        file_content_with_header = 'blob ' + str(file_content_size) + '\x00' + file_content
+        file_content_size = len(file_content_bytes)
+        header_bytes = f"blob {file_content_size}\x00".encode('ascii')
+        file_content_with_header = header_bytes + file_content_bytes
         sha1_of_file = self._compute_sha1_hash(file_content_with_header)
-        compressed_data = zlib.compress(file_content_with_header.encode('utf-8'), level=9) 
+        compressed_data = zlib.compress(file_content_with_header, level=9) 
         
         # priting the hash to stdout
         print(sha1_of_file)
 
         # making dir in object using the hash result
-        path_new_object_dir = os.path.join(self.git_dir, 'objects', sha1_of_file[:2])
+        path_new_object_dir = os.path.join(self.git_dir, Git.OBJECTS_DIR , sha1_of_file[:2])
         os.makedirs(path_new_object_dir, exist_ok=True)
 
         # writing to the hash defined path
@@ -107,11 +110,10 @@ class Git:
             return None
         
     # Computing sha1 hash 
-    def _compute_sha1_hash(self, input_string): 
-        # create object of hashlib -> update with utf encoded file data -> hexdigest function
-
+    def _compute_sha1_hash(self, input_bytes: bytes): 
+        # create object of hashlib -> update with encoded file data -> hexdigest function
         sha1_hash = hashlib.sha1()
-        sha1_hash.update(input_string.encode('utf-8'))
+        sha1_hash.update(input_bytes)
 
         return sha1_hash.hexdigest()
 
