3ebbd504a6bceba9403a1d6fb79a7bc237ea6dc4
Utsav Goyal <goyalutsav2004@gmail.com>
2025-09-26 23:57:11 +0530

    Refacotoring - refining dirty messy code to a more readable one

--- app/main.py
+++ app/main.py
@@ -4,102 +4,122 @@ import zlib
 import hashlib
 
 class Git:
+    # hard coding some name for reusability across class - ALL_CAPS is a convention for variable name for a class
+    OBJECTS_DIR = 'objects'
+    REFS_DIR = 'refs'
+    HEAD_FILE = 'HEAD'
+    HEADS_DIR = 'heads'
+
     def __init__(self, git_dir ='.git'):
         self.git_dir = git_dir
 
-    def init(self): # Initialising a new Git repo
-        # creating necessary directories
-        os.makedirs(os.path.join(self.git_dir, 'objects'), exist_ok=True)
-        os.makedirs(os.path.join(self.git_dir, 'refs', 'heads'), exist_ok=True)
+    # -------- INITIALISING GIT ---------
+
+    def init(self): 
+        # creating necessary directories - objects and refs/heads
+        os.makedirs(os.path.join(self.git_dir, Git.OBJECTS_DIR), exist_ok=True)
+        os.makedirs(os.path.join(self.git_dir, Git.REFS_DIR, Git.HEADS_DIR), exist_ok=True)
 
         # writing to head file
-        with open(".git/HEAD", 'w') as f:
-            f.write("ref: refs/heads/main\n")
+        with open(os.path.join(self.git_dir, Git.HEAD_FILE), 'w') as f:
+            f.write(f"ref: {os.path.join(Git.REFS_DIR, Git.HEADS_DIR)}/main\n")
 
         print("Initialized git directory")
 
-    def cat_file(self, args): # implementing git cat-file <flag> function
+
+    # -------- GIT COMMANDS --------
+
+    # Command : git cat-file <flag> <hash-of-the-file>
+    def cat_file(self, args): 
         if len(args) < 2:
             print(f"Usage: cat-file <flag> <hash>", file=sys.stderr)
             sys.exit(1)
 
-        hash_str = args[1]
-
-        object_path = os.path.join(self.git_dir, 'objects', hash_str[:2], hash_str[2:])
-
         flag = args[0]
+        hash_str = args[1]
+        object_path = os.path.join(self.git_dir, 'objects', hash_str[:2], hash_str[2:]) # the object associated with the given hash
         
+        # Need content of file at the path - zlib decompression
+        content = self._get_object_content(object_path)
+
+        # Content format - <object-type> <size>\x00<content>
+        if content:
+            header, _ , body = content.partition(b'\x00')
+        else:
+            print("Error with reading file")
+            sys.exit(1)
+            
         if flag == '-p':
-            content = self._get_object_content(object_path)
-
-            # now this content has the object type and the file size after decompression
-            # we need to parse this as well
-            if content:
-                header, _ , body = content.partition(b'\x00')
-                print(body.decode('utf-8'), end='')
-
+            print(body.decode('utf-8'), end='') # content in byte format - converting to string format
         else:
             print("Usage: cat-file <flag> <hash-of-object>", file=sys.stderr)
 
-    def hash_object(self, args): # implementing git hash-object flag file-name
+    # Command: git hash-object <flag> <file-name>
+    def hash_object(self, args):
         if len(args) < 2 or args[0] != '-w':
             print(f"Usage: hash-object -w file-name", file=sys.stderr)
 
         if not os.path.exists(args[1]):
             print(f"fatal: file does not exist.")
 
-
+        # Need to : Read file -> Add header to file -> Calculate the sha1 hash -> compress file using zlib -> write to the objects/hash[:2] path
         try:
             with open(args[1], 'r') as f:
                 file_content = f.read()
-
-            # now we need to add header to the data before calculating the hash 
-            content_size = os.path.getsize(args[1])
-            content_to_hash = 'blob ' + str(content_size) + '\x00' + file_content
-
-            sha1_result = self._compute_sha1_hash(content_to_hash)
-
         except Exception as e:
-            print(f"Error occured: {e}", file=sys.stderr)
+            print(f"Error in reading the file: {e}", file=sys.stderr)
             sys.exit(1)
 
-        path_to_object_dir = os.path.join(self.git_dir, 'objects', sha1_result[:2])
-        os.makedirs(path_to_object_dir, exist_ok=True)
+        file_content_size = os.path.getsize(args[1])
+        file_content_with_header = 'blob ' + str(file_content_size) + '\x00' + file_content
+        sha1_of_file = self._compute_sha1_hash(file_content_with_header)
+        compressed_data = zlib.compress(file_content_with_header.encode('utf-8'), level=9) 
+        
+        # priting the hash to stdout
+        print(sha1_of_file)
 
-        path_to_object = os.path.join(path_to_object_dir, sha1_result[2:])
-   
-        compressed_data = zlib.compress(content_to_hash.encode('utf-8'), level=9) 
+        # making dir in object using the hash result
+        path_new_object_dir = os.path.join(self.git_dir, 'objects', sha1_of_file[:2])
+        os.makedirs(path_new_object_dir, exist_ok=True)
 
+        # writing to the hash defined path
+        path_new_object = os.path.join(path_new_object_dir, sha1_of_file[2:])
         try:
-            with open(path_to_object, 'wb') as f:
+            with open(path_new_object, 'wb') as f:
                 f.write(compressed_data)
         except Exception as e:
             print(f"Error while writing to file: {e}", file=sys.stderr)
+            sys.exit(1)
 
-        print(sha1_result)
+    # -------- HELPER FUNCTIONS --------
 
-    def _compute_sha1_hash(self, input_string): # dedicated function to create sha1 hash for a string
-        sha1_hash = hashlib.sha1()
-
-        sha1_hash.update(input_string.encode('utf-8'))
-
-        return sha1_hash.hexdigest()
-
+    # Reading and Decompress a zlib-compressed object file
     def _get_object_content(self, file_path):
-        # a helper method to read and decompress a zlib-compressed object file
         if not os.path.exists(file_path):
             return None
         
         try:
-            with open(file_path, 'rb') as f:
+            with open(file_path, 'rb') as f: # zlib-compressed - Binary format
                 compressed_data = f.read()
             return zlib.decompress(compressed_data)
 
         except (zlib.error, Exception):
             return None
+        
+    # Computing sha1 hash 
+    def _compute_sha1_hash(self, input_string): 
+        # create object of hashlib -> update with utf encoded file data -> hexdigest function
+
+        sha1_hash = hashlib.sha1()
+        sha1_hash.update(input_string.encode('utf-8'))
+
+        return sha1_hash.hexdigest()
+
+# -------- MAIN FUNCTION --------
 
 def main():
 
+    # Exiting for the case when no command passed with the _.sh exectuion 
     if len(sys.argv) < 2:
         print(f"Usage: <command> [args]", file=sys.stderr)
         sys.exit(1)
@@ -107,6 +127,7 @@ def main():
     command = sys.argv[1]
     arguments = sys.argv[2:]
 
+    # Creating an object of the Git class
     git = Git()
 
     if command == "init":
