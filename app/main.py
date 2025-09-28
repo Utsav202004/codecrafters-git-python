f22c03796d7dc20017cde2bf0ba411a51969d1e4
Utsav Goyal <goyalutsav2004@gmail.com>
2025-09-28 12:46:03 +0530

    Write a tree object - Implementing git write-tree command, bypassing the stage command, manually adding all blobs and tree and sub blobs and sub tree of a tree - fix 9 - making the directory for tree before writing

--- app/main.py
+++ app/main.py
@@ -176,7 +176,9 @@ class Git:
 
             tree_sha = self._compute_sha1_hash(tree_object)
 
-            path_to_tree_object = os.path.join(self.git_dir, Git.OBJECTS_DIR, tree_sha[:2], tree_sha[2:])
+            path_to_tree_dir = os.path.join(self.git_dir, Git.OBJECTS_DIR, tree_sha[:2])
+            os.makedirs(path_to_tree_dir)
+            path_to_tree_object = os.path.join(path_to_tree_dir, tree_sha[2:])
 
             try:
                 with open(path_to_tree_object, 'wb') as f:
